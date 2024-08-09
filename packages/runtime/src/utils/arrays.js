296e2b7710cb313017b8addc6d93718c6cb4fa03
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2024-08-09 23:12:46 +0700

    handle addition, move, remove outstanding items cases in arrays diff sequence

--- packages/runtime/src/utils/arrays.js
+++ packages/runtime/src/utils/arrays.js
@@ -73,6 +73,69 @@ class ArrayWithOrginalIndices {
       item: this.#array[index]
     }
   }
+
+  isAddition(item, fromIdx) {
+    return this.findIndexFrom(item, fromIdx) === - 1
+  }
+
+  findIndexFrom(item, fromIndex) {
+    for (let i = fromIndex; i < this.length; i++) {
+      if (this.#equalsFn(item, this.#array[i])) {
+        return i
+      }
+
+      return -1
+    }
+  }
+
+  addItem(item, index) {
+    // creates the add operation
+    const operation  ={
+      op: ARRAY_DIFF_OP.ADD,
+      index, 
+      item
+    }
+    // adds the new item to theold array add the given index
+    this.#array.splice(index, 0, item)
+    // adds a -1 index to originalIndices array at the given index
+    this.#originalIndices.splice(index, 0, -1)
+
+    return operation
+  }
+
+  moveItem(item, toIndex) {
+    // looks for item in the old array, starting from the target index
+    const fromIndex = this.findIndexFrom(item, toIndex)
+
+    const operation = {
+      op: ARRAY_DIFF_OP.MOVE,
+      originalIndex: this.originalIndexAt(fromIndex),
+      from: fromIndex,
+      index: toIndex,
+      item: this.#array[fromIndex]
+    }
+
+    // extracts item from the old array
+    const [_item] = this.#array.splice(fromIndex, 1)
+    // inserts item into the new position
+    this.#array.splice(toIndex, 0, _item)
+
+    // extracts original index from the originalIndices array
+    const [originalIndex] = this.#originalIndices.splice(fromIndex, 1)
+    this.#originalIndices.splice(toIndex, 0, originalIndex)
+
+    return operation
+  }
+
+  removeItemsAfter(index) {
+    const operations = []
+
+    while (this.length > index) {
+      operations.push(this.removeItem(index))
+    }
+
+    return operations
+  }
 }
 
 export function withoutNulls(arr) {
@@ -109,9 +172,20 @@ export function arraysDiffSequence(oldArray, newArray, equalsFn = (a, b) => a ==
       continue
     }
 
-    // TODO: addition case
-    // TODO: move case
+    // get items in the new array at the current index
+    const item = newArray[index]
+
+    // check whether the case is an addition
+    if (array.isAddition(item, index)) {
+      // appends the add operation to the sequence
+      sequence.push(array.addItem(item, index))
+      continue
+    }
+
+    sequence.push(array.moveItem(item, index))
   }
+
+  sequence.push(...array.removeItemsAfter(newArray.length))
 
   return sequence 
 }
